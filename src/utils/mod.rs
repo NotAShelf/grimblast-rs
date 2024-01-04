8fefcfb35e23eaa66c9b92db7f8ee75774299f10
NotAShelf <raf@notashelf.dev>
2024-01-04 09:50:12 +0300

    utils: get_active() -> get_active_region()

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,7 +4,7 @@ pub mod notify;
 pub mod proc;
 pub mod screenshot;
 
-pub use actions::get_active;
+pub use actions::{get_active_region,get_focused_monitor};
 pub use dirs::{env_editor_confirm, get_target_directory, tmp_editor_directory};
 pub use notify::{notify, notify_error, notify_ok};
 pub use proc::{check, die, kill_hyprpicker, wait};
